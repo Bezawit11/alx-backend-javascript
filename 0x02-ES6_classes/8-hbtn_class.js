35d0107629222b7da36e39090e8fe3de45668053
Bezawit11 <socbeza13@gmail.com>
2023-04-20 21:24:53 +0000

    task 9-11

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+}
+[Symbol.toPrimitive](hint){
+    switch (hint) {
+      case 'string':
+        return this._location;
+
+      case 'number':
+        return this._size;
+
+    }
+  }
+}
