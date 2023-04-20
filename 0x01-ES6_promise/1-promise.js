cd6d912409d2cd5c24e80a6adea72ab78884ec9a
Bezawit11 <socbeza13@gmail.com>
2023-04-20 09:18:29 +0000

    task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success) {
+	let p = new Promise(function (resolve, reject) {
+		if (success == true) {
+			resolve({status: 200,
+				body: 'Success'})
+		}
+		else
+			reject('The fake API is not working currently')
+		})
+		return p;
+}
