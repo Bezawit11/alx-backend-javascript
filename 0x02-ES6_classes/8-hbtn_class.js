25dce6379ad948362b99555605b45311a08b6218
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-04-21 12:19:21 +0100

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -2,15 +2,15 @@ export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
-}
-[Symbol.toPrimitive](hint){
+  }
+  [Symbol.toPrimitive] (hint){
     switch (hint) {
       case 'string':
         return this._location;
-
       case 'number':
         return this._size;
-
+      default:
+        break;
     }
   }
 }
