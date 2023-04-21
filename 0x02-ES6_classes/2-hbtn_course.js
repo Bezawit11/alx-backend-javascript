faca19aaff7c6fba98c3e23caa324ffa8a6c1130
Bezawit11 <socbeza13@gmail.com>
2023-04-21 10:02:41 +0000

    eslint fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,24 +2,30 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
-    this._students = students; 
-}
-  set name(newName) {
-      this._name = newName
-}
-  get name() {
-      return this._name
+    this._students = students;
   }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
   set length(newLength) {
-      this._length = newLength
-}
- get length() {
-     return this._length
- }
- set students(newStudent) {
-      this._students = newStudent
-}
- get students() {
-     return this._students
- }
+    this._length = newLength;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(newStudent) {
+    this._students = newStudent;
+  }
+
+  get students() {
+    return this._students;
+  }
 }
