3a84148d6ede8515a8ad7347e76967567f2d8af3
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-04-23 07:46:43 +0100

    Update 4-rest-parameter.js

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,6 @@
 export default function returnHowManyArguments(...theArgs) {
   let total = 0;
-  for (const arg of theArgs) {
-    let b = arg;
+  for (const foo of theArgs) {
     total += 1;
   }
   return total;
