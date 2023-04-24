b3403b21d1717c65664c20c1b0a39d8600b3b26b
Bezawit11 <socbeza13@gmail.com>
2023-04-24 07:56:28 +0000

    task 3 n 4

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,6 @@
 export default function returnHowManyArguments(...theArgs) {
   let total = 0;
-  for (const foo of theArgs) {
+  for (const foo of theArgs) { // eslint-disable-line no-unused-vars
     total += 1;
   }
   return total;
