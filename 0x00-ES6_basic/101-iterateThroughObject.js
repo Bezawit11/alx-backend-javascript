24deff8a181b1ffdef3bac61bc1d15b71a71ccfb
Bezawit11 <socbeza13@gmail.com>
2023-04-24 12:35:09 +0000

    task 101

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,12 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let s = '';
+  const arr = reportWithIterator.length;
+  for (let index = 0; index < arr; index++) { // eslint-disable-line no-plusplus
+    if (index === arr - 1) {
+      s += ''.concat(reportWithIterator[index]);
+    } else {
+      s += ''.concat(reportWithIterator[index], '|');
+    }
+  }
+  return s;
+}
