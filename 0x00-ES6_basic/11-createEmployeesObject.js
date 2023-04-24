1da402baa24e1ca032b4916ed91a0e84334ee0de
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-04-24 08:28:36 +0100

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-	let o = {}
-	var dep = departmentName;
-	o[dep] = employees;
-	return o;
+  var dep = departmentName;
+  o = {dep: employees};
+  return o;
 }
