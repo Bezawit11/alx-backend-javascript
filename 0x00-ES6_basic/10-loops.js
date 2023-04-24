c1a7b367203585d817ebff40ca9545d17f9c4786
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-04-24 08:09:52 +0100

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
   for (const value of array) {
-    array[i] = appendString + value;
+    array[i] = appendString + value; // eslint-disable-line no-param-reassign
     i += 1;
   }
   return array;
