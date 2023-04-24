00587095dd9f6010798cd9dae412b8b81c9742f0
Bezawit11 <socbeza13@gmail.com>
2023-04-24 07:51:30 +0000

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,12 @@
 export default function createReportObject(employeesList) {
-	return {
-		allEmployees: employeesList,
-		getNumberOfDepartments(employees) {
-		var count = 0;
-		for(const [key, value] of Object.entries(employees)){
-			count += 1;
-		}
-		return count;
-		}
-	};
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employees) {
+      let count = 0;
+      for (const [key, value] of Object.entries(employees)) { // eslint-disable-line no-unused-vars
+        count += 1;
+      }
+      return count;
+    },
+  };
 }
