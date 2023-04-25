8359ba23f7313459b6157341ba9beb6d10e742db
Bezawit11 <socbeza13@gmail.com>
2023-04-25 11:20:04 +0000

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(s, start) {
   let k = '';
+  if (start === '') {
+    return '';
+  }
   for (let i of s) {
     if (i.startsWith(start) === true) {
-      s += ''.concat(i, '-');
+      k += ''.concat(i.replace(start, ''), '-');
     }
   }
-  return s;
+  return k.substring(0, k.length-1);;
 }
