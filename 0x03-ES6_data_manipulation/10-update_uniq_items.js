ed2ae7221c1eaabb7877acb2b5c31d2bbbbf1584
Bezawit11 <socbeza13@gmail.com>
2023-04-25 11:56:56 +0000

    task 9 n 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,8 @@
 export default function updateUniqueItems(m) {
-  if (Object.getPrototypeOf(map) !== Map.prototype) {
+  if (Object.getPrototypeOf(m) !== Map.prototype) {
     throw Error('Cannot process');
   }
-  for (let key of m.keys()) {
+  for (const key of m.keys()) {
     if (m.get(key) === 1) {
       m.set(key, 100);
     }
