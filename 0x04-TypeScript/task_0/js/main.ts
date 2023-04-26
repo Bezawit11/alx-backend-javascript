7e4984a7fb3f12ffc3abb9017ce919a180771601
Bezawit11 <socbeza13@gmail.com>
2023-04-26 08:22:40 +0000

    task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+  };
+
+const student1: Student = {
+  firstName: "Beza",
+  lastName: "Bekele",
+  age: 23,
+  location: "Addis Ababa",
+  };
+ 
+const student2: Student = {
+  firstName: "Azeb",
+  lastName: "Belay",
+  age: 23,
+  location: "Addis Ababa",
+  };
+
+const a = [student1, student2];
