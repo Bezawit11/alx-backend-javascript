1f0d734c63c6bb92ddaf7697a9f50d7999caa761
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-16 11:55:47 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
+    .then(() => { // eslint-disable-line arrow-body-style
       return ({ status: 200, body: 'success' });
     })
-    .catch(() => { return new Error();
+    .catch(() => { return new Error(); // eslint-disable-line brace-style arrow-body-style
     })
-    .finally(() => { console.log('Got a response from the API');
-    })
-};
+    .finally(() => { console.log('Got a response from the API'); // eslint-disable-line brace-style
+    });
+}
