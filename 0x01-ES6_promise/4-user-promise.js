eec05d104f2e1fdba70644a579efb09aad473a25
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-16 12:41:21 +0100

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
 export default function signUpUser(firstName, lastName) {
-  return  new Promise((resolve, reject)=>{    
-    resolve({ 'firstName': firstName, 'lastName': lastName });   
-})  
+  return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
+    resolve({ firstName: firstName, lastName: lastName });
+  });
 }
