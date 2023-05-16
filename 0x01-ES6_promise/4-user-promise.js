dcae7322faeff695007afdc3584136b931f686bc
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-16 12:27:10 +0100

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,6 @@
 export default function signUpUser(firstName, lastName) {
    return new Promise(() => {
-     firstName: firstName,
-     lastName: lastName,
+     'firstName': firstName,
+     'lastName': lastName,
    });
-});
 }
