1428bbed74dd5758a7c9c8b1cbea2f9371e9a6c3
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-13 16:12:14 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,13 +2,18 @@
  *takes input from user on console
  */
 
-const readline = require('readline').createInterface({
+const readline = require("readline");
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', name => {
+rl.question("Welcome to Holberton School, what is your name?\n", function(name) {
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing')
-  readline.close();
+  rl.close();
+});
+
+rl.on("close", function() {
+  console.log("This important software is now closing");
+  process.exit(0);
 });
