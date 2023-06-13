e996e30550dea67cce5128f0aca4e92f2b2c403b
Bezawit11 <socbeza13@gmail.com>
2023-06-13 14:57:47 +0000

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+/**
+ *takes input from user on console
+ */
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Welcome to Holberton School, what is your name?\n', name => {
+  console.log(`Your name is: ${name}`);
+	console.log('This important software is now closing')
+  readline.close();
+});
