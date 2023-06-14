f8ebcd3abf4c3314afae98c432f7cb88f5a4e59e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-14 12:49:10 +0100

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,7 +3,7 @@
  */
 const fs = require('fs');
 const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, {encoding: 'utf-8'}, (err, data) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
   if (err) {
     reject(new Error('Cannot load the database'));
   }
