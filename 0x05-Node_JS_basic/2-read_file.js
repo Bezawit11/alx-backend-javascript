5c82b853cf8c12446fcdb1acbb5417533de0842a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-14 10:02:44 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 const countStudents = (path) => {
   try {
   fs.readFileSync(path, function(err, data) {
-  console.log('success');
+  console.log(data);
   });
   } catch (err) {
   throw new Error('Cannot load the database');
