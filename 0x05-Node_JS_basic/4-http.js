b9154c341058d874b12f1463314bf7932e3d1e41
Bezawit11 <socbeza13@gmail.com>
2023-06-14 13:07:00 +0000

    task 4 eslint fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
 /**
  *creating a server object
- **/
+ * */
 const http = require('http');
 
-const app = http.createServer(function (req, res) {
+const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!');
   res.end();
-}).listen(1245); 
+}).listen(1245);
 
 module.exports = app;
