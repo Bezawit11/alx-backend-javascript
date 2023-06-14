1c8b90c089bdbbcd8e91d496e5647d9b6c5031e2
Bezawit11 <socbeza13@gmail.com>
2023-06-14 08:52:04 +0000

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,14 @@
+/**
+ * displays message on the STDOUT.
+ */
+var fs = require('fs');
+const countStudents = (path) => {
+  try {
+  fs.readFileSync(path, function(err, data) {
+  console.log(data);
+  });
+  } catch (err) {
+  console.log('Cannot load the database');
+  }
+};
+module.exports = countStudents;
