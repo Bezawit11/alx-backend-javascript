1fc1c9ed577f94f76a6c876d5c5cbc3aa8743ce1
Bezawit11 <socbeza13@gmail.com>
2023-06-14 11:27:09 +0000

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+/**
+ * displays message on the STDOUT.
+ */
+const fs = require('fs');
+const countStudents = (path) => {
+  try {
+  const data =  fs.readFileSync(path, 'utf-8');
+  let s = data.split('\n');
+  s.shift()
+  s.pop()
+  s = s.reverse()
+  console.log('Number of students:', s.length);
+  var dict = {};
+  for (let i = 1; i <= s.length; i++) {
+    let g = s[s.length - i].split(',');
+    var m = ' ' + g[0];
+    if (dict[g[g.length - 1]] === undefined) {
+      var k = [m]
+      dict[g[g.length - 1]] = k;
+    }
+    else {
+      var l = dict[g[g.length - 1]];
+      l.push(m)
+      dict[g[g.length - 1]] = l;
+    }
+  }
+  for(var key in dict) {
+   console.log('Number of students in ' + key + ": " + dict[key].length + '.' + ' List:' + dict[key]);
+}
+  } catch (err) {
+  throw new Error('Cannot load the database');
+  }
+};
+module.exports = countStudents;
