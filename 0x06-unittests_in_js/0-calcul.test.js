2ba72d9472174cf148066c9e91ff6c9af2849247
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-15 09:25:03 +0100

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe("Test calculateNumber function", () => {
-  it("add two numbers", () => {
-    assert.strictEqual(calculateNumber(2, 2), 4);
+  it("add two integer numbers", () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it("add one integer and one floating number", () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it("add two floating numbers", () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it("add two floating numbers", () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
