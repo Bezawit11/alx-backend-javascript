d8d583d8024f873cf08a3a0b0e116297b08f0dfb
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-15 13:37:54 +0100

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,7 +7,7 @@ describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
     const a_spy = sinon.spy(Utils);
     sendPaymentRequestToApi(100, 20);
-    expect( a_spy.called ).to.be.true;
-    expect(a_spy.calculateNumber.callCount).to.equal(1);
+    expect( a_spy.calculateNumber.called ).to.be.true();
+    expect(a_spy.calculateNumber.callCount).to.be.equal(1);
   });
 };
