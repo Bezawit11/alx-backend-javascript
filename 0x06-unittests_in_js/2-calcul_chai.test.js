8156e69231f96f0f0236a07fecaa7ec5958820d9
Bezawit11 <socbeza13@gmail.com>
2023-06-15 10:14:54 +0000

    task 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe("Test calculateNumber function", () => {
+  it("add two integer numbers", () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+  });
+  it("add one integer and one floating number", () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+  });
+  it("add two floating numbers", () => {
+    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+  });
+  it("add two floating numbers", () => {
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+  });
+  it("add two floating numbers", () => {
+    assert.strictEqual(calculateNumber('SUM', 1.49, 3.49), 4);
+  });
+  it("subtract two integer numbers", () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 3, 1), 2);
+  });
+  it("subtract two floating numbers", () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 3.49, 1.49), 2);
+  });
+  it("subtract one integer and one floating number", () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+  it("divide two integer numbers", () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it("divide 0 by one integer", () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 0, 1.4), 0);
+  });
+  it("divide 0 by one integer", () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+});
