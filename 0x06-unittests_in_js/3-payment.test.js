75ad634d80cb9b5fc54b14437268d63ecd3589d6
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-15 13:34:00 +0100

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,6 +2,12 @@ const sinon = require("sinon");
 const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment.test');
-var a_spy = sinon.spy(Utils.calculateNumber('SUM', 100, 20));
-var b_spy = sinon.spy(sendPaymentRequestToApi(100, 20));
-expect( a_spy ).to.equal( b_spy );
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const a_spy = sinon.spy(Utils);
+    sendPaymentRequestToApi(100, 20);
+    expect( a_spy.called ).to.be.true;
+    expect(a_spy.calculateNumber.callCount).to.equal(1);
+  });
+};
