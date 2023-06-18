769a5fbdd0e0367c60fd37c3c4dc21911f2e7270
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-18 14:16:55 +0100

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,8 +5,42 @@
  *creating a server object
  * */
 const http = require('http');
+const fs = require('fs');
 const host = 'localhost';
 const port = 1245;
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      let s = data.split('\n');
+      s.shift();
+      s.pop();
+      s = s.reverse();
+      console.log('Number of students:', s.length);
+      const dict = {};
+      for (let i = 1; i <= s.length; i++) {
+        const g = s[s.length - i].split(',');
+        const m = g[0];
+        if (dict[g[g.length - 1]] === undefined) {
+          const k = [m];
+          dict[g[g.length - 1]] = k;
+        } else {
+          const l = dict[g[g.length - 1]];
+          l.push(m);
+          dict[g[g.length - 1]] = l;
+        }
+      }
+      for (const key in dict) {
+        console.log(`Number of students in ${key}: ${dict[key].length}.` + ` List: ${dict[key].join(', ')}`);
+      }
+      resolve(true);
+    });
+  });
+}
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
     switch (req.url) {
@@ -15,6 +49,13 @@ const requestListener = function (req, res) {
             break
         case "/students":
             res.end("This is the list of our students");
+            countStudents("database.csv")
+              .then(() => {
+                console.log("Done!");
+            })
+            .catch((error) => {
+            console.log(error);
+    });
             break
     }
 }
