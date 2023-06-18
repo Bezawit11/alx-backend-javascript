3bcc846f2fc231fd19b509b1c6a60779985657df
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-18 13:12:58 +0100

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -16,7 +16,7 @@ function countStudents(path) { return new Promise((resolve, reject) => {
   var dict = {};
   for (let i = 1; i <= s.length; i++) {
     let g = s[s.length - i].split(',');
-    var m = g[0]; // eslint-disable-line no-plusplus
+    var m = g[0];  // eslint-disable-line no-plusplus
     if (dict[g[g.length - 1]] === undefined) {
       var k = [m]
       dict[g[g.length - 1]] = k;
@@ -28,8 +28,8 @@ function countStudents(path) { return new Promise((resolve, reject) => {
     }
   }
   for(var key in dict) {
-   console.log('Number of students in ' + key + ": " + dict[key].length + '.' + ' List: ' + dict[key].join(', ')); // eslint-disable-line guard-for-in
-} // eslint-disable-line no-useless-concat
+   console.log('Number of students in ' + key + ": " + dict[key].length + '.' + ' List: ' + dict[key].join(', '));  // eslint-disable-line guard-for-in
+}  // eslint-disable-line no-useless-concat
     resolve(true);
 });
 });
