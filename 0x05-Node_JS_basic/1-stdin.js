b7f996a5fe1de815691abbb00fb456afad35bb57
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-18 12:18:23 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@
  *takes input from user on console
  */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('read_input', () => {
+process.stdin.on('readable', () => {
   const user_input = process.stdin.read();
   if (user_input) {
     process.stdout.write(`Your name is: ${user_input}`);
