843b7b06c2dab5b5a2dcc0e4e9a7c38a68a1b678
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-18 12:00:00 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,6 +4,10 @@
 
 process.stdout.write("Welcome to Holberton School, what is your name?\n")
 process.stdin.on('data', data => {
-  console.log(`You typed ${data}`);
+  console.log(`Your name is: ${data}`);
   process.exit();
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
