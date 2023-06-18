51882fb0e1e24ef9c3a8ea42a76bb62d2eae67d7
Bezawit11 <socbeza13@gmail.com>
2023-06-18 12:17:43 +0000

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,39 @@
 /**
  * displays message on the STDOUT.
  */
-/* eslint-disable no-plusplus, guard-for-in, no-useless-concat*/
+/* eslint-disable no-plusplus, guard-for-in, no-useless-concat */
 const fs = require('fs');
-function countStudents(path) { return new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-  if (err) {
-    reject(Error('Cannot load the database'));
-    return;
-  }
-  let s = data.split('\n');
-  s.shift()
-  s.pop()
-  s = s.reverse()
-  console.log('Number of students:', s.length);
-  var dict = {};
-  for (let i = 1; i <= s.length; i++) {
-    let g = s[s.length - i].split(',');
-    var m = g[0];
-    if (dict[g[g.length - 1]] === undefined) {
-      var k = [m]
-      dict[g[g.length - 1]] = k;
-    }
-    else {
-      var l = dict[g[g.length - 1]];
-      l.push(m)
-      dict[g[g.length - 1]] = l;
-    }
-  }
-  for(var key in dict) {
-   console.log('Number of students in ' + key + ": " + dict[key].length + '.' + ' List: ' + dict[key].join(', '));
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      let s = data.split('\n');
+      s.shift();
+      s.pop();
+      s = s.reverse();
+      console.log('Number of students:', s.length);
+      const dict = {};
+      for (let i = 1; i <= s.length; i++) {
+        const g = s[s.length - i].split(',');
+        const m = g[0];
+        if (dict[g[g.length - 1]] === undefined) {
+          const k = [m];
+          dict[g[g.length - 1]] = k;
+        } else {
+          const l = dict[g[g.length - 1]];
+          l.push(m);
+          dict[g[g.length - 1]] = l;
+        }
+      }
+      for (const key in dict) {
+        console.log(`Number of students in ${key}: ${dict[key].length}.` + ` List: ${dict[key].join(', ')}`);
+      }
+      resolve(true);
+    });
+  });
 }
-    resolve(true);
-});
-});
-};
 module.exports = countStudents;
