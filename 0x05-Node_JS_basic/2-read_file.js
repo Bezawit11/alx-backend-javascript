fb8e3b1a6be2d8cf263b5f19fa6f3e130b04718d
Bezawit11 <socbeza13@gmail.com>
2023-06-18 11:36:09 +0000

    eslint fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,33 +2,33 @@
  * displays message on the STDOUT.
  */
 const fs = require('fs');
+
 const countStudents = (path) => {
   try {
-  const data =  fs.readFileSync(path, 'utf-8');
-  let s = data.split('\n');
-  s.shift()
-  s.pop()
-  s = s.reverse()
-  console.log('Number of students:', s.length);
-  var dict = {};
-  for (let i = 1; i <= s.length; i++) {
-    let g = s[s.length - i].split(',');
-    var m = ' ' + g[0];
-    if (dict[g[g.length - 1]] === undefined) {
-      var k = [m]
-      dict[g[g.length - 1]] = k;
+    const data = fs.readFileSync(path, 'utf-8');
+    let s = data.split('\n');
+    s.shift();
+    s.pop();
+    s = s.reverse();
+    console.log('Number of students:', s.length);
+    const dict = {};
+    for (let i = 1; i <= s.length; i++) {
+      const g = s[s.length - i].split(',');
+      const m = ` ${g[0]}`;
+      if (dict[g[g.length - 1]] === undefined) {
+        const k = [m];
+        dict[g[g.length - 1]] = k;
+      } else {
+        const l = dict[g[g.length - 1]];
+        l.push(m);
+        dict[g[g.length - 1]] = l;
+      }
     }
-    else {
-      var l = dict[g[g.length - 1]];
-      l.push(m)
-      dict[g[g.length - 1]] = l;
+    for (const key in dict) {
+      console.log(`Number of students in ${key}: ${dict[key].length}.` + ` List:${dict[key]}`);
     }
-  }
-  for(var key in dict) {
-   console.log('Number of students in ' + key + ": " + dict[key].length + '.' + ' List:' + dict[key]);
-}
   } catch (err) {
-  throw new Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 };
 module.exports = countStudents;
