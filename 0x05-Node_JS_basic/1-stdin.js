641e06333d4049b20ea7377001cbcfff49b40aee
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-18 11:58:19 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,16 +2,8 @@
  *takes input from user on console
  */
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n")
+process.stdin.on('data', data => {
+  console.log(`You typed ${data}`);
+  process.exit();
 });
