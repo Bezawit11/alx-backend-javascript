e365b5c257bf83e8bc980fd14130858632533913
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-18 14:08:12 +0100

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,16 +1,26 @@
 /**
  *creating a server object
  * */
+/**
+ *creating a server object
+ * */
 const http = require('http');
+const host = 'localhost';
+const port = 1245;
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/":
+            res.end("Hello Holberton School!");
+            break
+        case "/students":
+            res.end("This is the list of our students");
+            break
+    }
+}
 
-let options = {
-    host:'',
-    path: '/students',
-    method: 'GET'
-};
-const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+const app = http.createServer(requestListener);
+app.listen(port, host, () => {
+});
 
 module.exports = app;
