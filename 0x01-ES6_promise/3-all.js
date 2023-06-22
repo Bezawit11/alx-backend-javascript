d0a0cc16648c9371d4b32d14cb7cd33220f1b3a5
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-22 12:28:18 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,15 @@ import uploadPhoto from "./utils";
 import createUser from "./utils";
 
 export function handleProfileSignup() {
-  const promise = createUser();
-  promise.then((message) => {
-    console.log(message);
+  const promise1 = createUser();
+  const promise2 = uploadPhoto();
+  promise2.then((message) => {
+    process.stdout.write(message["body"] + " ");
+  }).catch((message) => {
+    console.log("Signup system offline");
+  })
+  promise1.then((message) => {
+    console.log(message["firstName"] + " " + message["lastName"]);
   }).catch((message) => {
     console.log("Signup system offline");
   })
