9ab55cf83ac278f63da23f624a9359cc71cff5b0
Bezawit11 <socbeza13@gmail.com>
2023-06-22 13:40:00 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((value) => {
-       console.log(value.map((v) => ({
+      console.log(value.map((v) => ({
         status: v.status,
-        value: v.status === 'fulfilled' ? v.value : String(v.reason)
-      })))
+        value: v.status === 'fulfilled' ? v.value : String(v.reason),
+      })));
     });
 }
