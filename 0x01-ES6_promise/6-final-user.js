dfed27bf902d1b8dca7fe4aa15e2d91b4e48fd09
Bezawit11 <socbeza13@gmail.com>
2023-06-22 13:12:35 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((value) => {
       console.log(value);
-    })
+    });
 }
