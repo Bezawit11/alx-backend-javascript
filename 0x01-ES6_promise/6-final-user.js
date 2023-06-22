1364b0c07cfee6659207142ca3ab54c28356a23b
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-22 14:23:03 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
+  const arr = [];
+  const d = {};
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((value) => {
-      console.log(value);
+       value.map((v) => ({
+        status: v.status,
+        value: v.status === 'fulfilled' ? v.value : String(v.reason),
+      }))
     });
 }
