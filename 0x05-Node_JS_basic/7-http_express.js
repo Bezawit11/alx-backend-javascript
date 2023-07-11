65bf24da013ba9be46101a0680d748128a7b2bc1
Bezawit11 <socbeza13@gmail.com>
2023-07-11 07:17:53 +0000

    task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+const DATABASE = args[0];
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+   res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE); // eslint-disable-line
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+});
+
+app.listen(1245, () => {
+});
+
+module.exports = app;
