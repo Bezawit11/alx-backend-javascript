87dc77cf0b9be6d6b8036dc13c2ef5d18c1a41b5
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-07-16 12:58:43 +0100

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,20 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./3-payment');
+const sinon = require('sinon');
 
-describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
 
+describe('sendPaymentRequestToApi function', () => {
+  // create the spy / wrapper of an existing function
+  const utilSpy = sinon.spy(Utils, 'calculateNumber');
+
+  it('validate the usage of the Utils function', () => {
+    // run the function with the parameters
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    // check that the spy was called exactly once
+    expect(utilSpy.calledOnce).to.be.true;
+    // check that spy was called at least once with the provided arguments
+    expect(utilSpy.calledWith('SUM', 100, 20)).to.be.true;
+    utilSpy.restore()
   });
 });
